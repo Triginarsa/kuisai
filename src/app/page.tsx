5201b0f3f67146212ac358131a4b01f1f16bb217
triginarsa <mankadi123@gmail.com>
2023-08-15 07:21:55 +0800

    💄 homepage 🚧 dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
+import SignInButton from "@/components/SignInButton";
 import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
+import { getAuthSession } from "@/lib/nextauth";
 
-export default function Home() {
-  return <Button>Hello World</Button>;
+export default async function Home() {
+  const session = await getAuthSession();
+  if (session?.user) {
+    return redirect("/dashboard");
+  }
+  return (
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to KuisAI! 🔥</CardTitle>
+          <CardDescription>
+            KuisAI is a quiz app that allow you to create and share quiz to your
+            beloved friends.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google!" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
