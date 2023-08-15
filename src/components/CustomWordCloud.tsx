0d7e7af480cd381bf772791e4680ecf323a12883
triginarsa <mankadi123@gmail.com>
2023-08-15 23:10:02 +0800

    feat: ✨ world cloud

---
+++ src/components/CustomWordCloud.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useTheme } from "next-themes";
+import React from "react";
+import D3WordCloud from "react-d3-cloud";
+
+type Props = {};
+
+const data = [
+  {
+    text: "Hey",
+    value: 3,
+  },
+  {
+    text: "hoi",
+    value: 5,
+  },
+  {
+    text: "hai",
+    value: 10,
+  },
+  {
+    text: "bonzua",
+    value: 5,
+  },
+];
+
+const fontSizeMapper = (word: { value: number }) => {
+  return Math.log2(word.value);
+};
+
+const CustomWordCloud = (props: Props) => {
+  const theme = useTheme();
+  return (
+    <>
+      <D3WordCloud
+        height={650}
+        data={data}
+        font="Times"
+        fontSize={fontSizeMapper}
+        rotate={0}
+        padding={10}
+        fill={theme.theme == "dark" ? "white" : "black"}
+      />
+    </>
+  );
+};
+
+export default CustomWordCloud;
