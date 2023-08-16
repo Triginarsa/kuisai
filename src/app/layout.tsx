8765a27fe767f39adbd0a0329d3cae1e54d51d2f
triginarsa <mankadi123@gmail.com>
2023-08-16 09:48:37 +0800

    feat: 📦️ add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
+        <Analytics />
         <Providers>
           <Navbar />
           {children}
